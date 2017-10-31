d2d1bd61bacb0c8cbb5445b3115d8f06e8751629
justins83 <justins83.waze@gmail.com>
2017-10-31 09:46:08 -0400

    New Wazer images
    Dynamically pulling the correct Wazer image, by rank, from the new Wazers icons png

--- Me.js
+++ Me.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         WME Me
-// @version      0.0.8
+// @version      2017.10.31.01
 // @description  Adds a layer to WME that draws ME on the map!
 // @author       JustinS83 (Original author: Joshua M. Kriegshauser)
 // @include      https://www.waze.com/editor*
@@ -20,6 +20,7 @@
     var WMEMe_TempMarker = null;
     var settings = {};
 
+    var wazerMap = {};
 
     function WMEMe_ZoomEnd() {
         //console.log('WMEMe_ZoomEnd');
@@ -38,39 +39,20 @@
         WMEMe_Marker.moveTo(W.map.getLayerPxFromLonLat(W.map.center));
     }
 
-    function WMEMe_InstallIcon() {
-        OpenLayers.Icon=OpenLayers.Class({url:null,size:null,offset:null,calculateOffset:null,imageDiv:null,px:null,initialize:function(a,b,c,d){this.url=a;this.size=b||{w:20,h:20};this.offset=c||{x:-(this.size.w/2),y:-(this.size.h/2)};this.calculateOffset=d;a=OpenLayers.Util.createUniqueID("OL_Icon_");this.imageDiv=OpenLayers.Util.createAlphaImageDiv(a)},destroy:function(){this.erase();OpenLayers.Event.stopObservingElement(this.imageDiv.firstChild);this.imageDiv.innerHTML="";this.imageDiv=null},clone:function(){return new OpenLayers.Icon(this.url,
-	this.size,this.offset,this.calculateOffset)},setSize:function(a){null!=a&&(this.size=a);this.draw()},setUrl:function(a){null!=a&&(this.url=a);this.draw()},draw:function(a){OpenLayers.Util.modifyAlphaImageDiv(this.imageDiv,null,null,this.size,this.url,"absolute");this.moveTo(a);return this.imageDiv},erase:function(){null!=this.imageDiv&&null!=this.imageDiv.parentNode&&OpenLayers.Element.remove(this.imageDiv)},setOpacity:function(a){OpenLayers.Util.modifyAlphaImageDiv(this.imageDiv,null,null,null,null,
-	null,null,null,a)},moveTo:function(a){null!=a&&(this.px=a);null!=this.imageDiv&&(null==this.px?this.display(!1):(this.calculateOffset&&(this.offset=this.calculateOffset(this.size)),OpenLayers.Util.modifyAlphaImageDiv(this.imageDiv,null,{x:this.px.x+this.offset.x,y:this.px.y+this.offset.y})))},display:function(a){this.imageDiv.style.display=a?"":"none"},isDrawn:function(){return this.imageDiv&&this.imageDiv.parentNode&&11!=this.imageDiv.parentNode.nodeType},CLASS_NAME:"OpenLayers.Icon"});
-    }
+    function setIconImage(){
+        var r = W.loginManager.user.rank;
+        var canvas = document.createElement('canvas');
+        $(canvas).attr('width', wazerMap[r].width);
+        $(canvas).attr('height', wazerMap[r].height);
+            ctx = canvas.getContext('2d');
+        ctx.drawImage(image,
+                          wazerMap[r].x, wazerMap[r].y,   // X & Y at which to start copying
+                          wazerMap[r].width, wazerMap[r].height,   // Width and height to copy
+                          0, 0,     // Place the result at 0, 0 in the canvas,
+                          wazerMap[r].width, wazerMap[r].height); // Size to draw on the new canvas
+        var croppedImage = canvas.toDataURL();
 
-
-    function WMEMe_Install() {
-        loadSettings();
-        var layer = W.map.getLayersBy('uniqueName', '__WMEMe');
-        var newLayer = new OpenLayers.Layer.Markers('Me!', {
-            displayInLayerSwitcher: true,
-            uniqueName: '__WMEMe'
-        });
-
-        // For some reason, OpenLayers.Icon is missing?!?
-        if (!OpenLayers.Icon) {
-            WMEMe_InstallIcon();
-        }
-
-        WMEMe_Layer = newLayer;
-        var tween = new OpenLayers.Tween(OpenLayers.Easing.Linear.easeInOut);
-
-        I18n.translations[I18n.currentLocale()].layers.name['__WMEMe'] = 'Me!';
-
-        W.map.addLayer(newLayer);
-
-        newLayer.setVisibility(settings.layerEnabled);
-
-        WazeWrap.Interface.AddLayerCheckbox("display", "Me", settings.layerEnabled, function(enabled){newLayer.setVisibility(enabled); settings.layerEnabled = enabled; saveSettings();});
-
-
-        var icon = new OpenLayers.Icon('https://www.waze.com/assets-editorimg/live-wazeraba4a171da962f0a1ee4ba4f3fe9619b.png', new OpenLayers.Size(50,42), new OpenLayers.Pixel(-25,-42));
+        icon = new OpenLayers.Icon(croppedImage, new OpenLayers.Size(wazerMap[r].width,wazerMap[r].height), new OpenLayers.Pixel(-(wazerMap[r].width/2),-wazerMap[r].height));
 
         // Me! Text
         if(settings.showName)
@@ -86,6 +68,63 @@
         }});
 
         $(icon.imageDiv).click(function(){settings.showName=!settings.showName; SetName(settings.showName); saveSettings();});
+
+        W.map.events.register('movestart', null, function(e) {
+            var iconClone = icon.clone();
+            iconClone.setOpacity(0.2);
+            WMEMe_TempMarker = new OpenLayers.Marker(W.map.center, iconClone);
+            newLayer.addMarker(WMEMe_TempMarker);
+        });
+
+        WMEMe_Marker = new OpenLayers.Marker(W.map.center, icon);
+        newLayer.addMarker(WMEMe_Marker);
+    }
+
+    function WMEMe_InstallIcon() {
+        OpenLayers.Icon=OpenLayers.Class({url:null,size:null,offset:null,calculateOffset:null,imageDiv:null,px:null,initialize:function(a,b,c,d){this.url=a;this.size=b||{w:20,h:20};this.offset=c||{x:-(this.size.w/2),y:-(this.size.h/2)};this.calculateOffset=d;a=OpenLayers.Util.createUniqueID("OL_Icon_");this.imageDiv=OpenLayers.Util.createAlphaImageDiv(a)},destroy:function(){this.erase();OpenLayers.Event.stopObservingElement(this.imageDiv.firstChild);this.imageDiv.innerHTML="";this.imageDiv=null},clone:function(){return new OpenLayers.Icon(this.url,
+	this.size,this.offset,this.calculateOffset)},setSize:function(a){null!=a&&(this.size=a);this.draw()},setUrl:function(a){null!=a&&(this.url=a);this.draw()},draw:function(a){OpenLayers.Util.modifyAlphaImageDiv(this.imageDiv,null,null,this.size,this.url,"absolute");this.moveTo(a);return this.imageDiv},erase:function(){null!=this.imageDiv&&null!=this.imageDiv.parentNode&&OpenLayers.Element.remove(this.imageDiv)},setOpacity:function(a){OpenLayers.Util.modifyAlphaImageDiv(this.imageDiv,null,null,null,null,
+	null,null,null,a)},moveTo:function(a){null!=a&&(this.px=a);null!=this.imageDiv&&(null==this.px?this.display(!1):(this.calculateOffset&&(this.offset=this.calculateOffset(this.size)),OpenLayers.Util.modifyAlphaImageDiv(this.imageDiv,null,{x:this.px.x+this.offset.x,y:this.px.y+this.offset.y})))},display:function(a){this.imageDiv.style.display=a?"":"none"},isDrawn:function(){return this.imageDiv&&this.imageDiv.parentNode&&11!=this.imageDiv.parentNode.nodeType},CLASS_NAME:"OpenLayers.Icon"});
+    }
+
+    var ctx, icon, newLayer;
+    var image = new Image();
+    function WMEMe_Install() {
+        loadSettings();
+        var layer = W.map.getLayersBy('uniqueName', '__WMEMe');
+        newLayer = new OpenLayers.Layer.Markers('Me!', {
+            displayInLayerSwitcher: true,
+            uniqueName: '__WMEMe'
+        });
+
+        // For some reason, OpenLayers.Icon is missing?!?
+        if (!OpenLayers.Icon) {
+            WMEMe_InstallIcon();
+        }
+
+        wazerMap["0"] = {x:67, y:62, width:67, height:47};
+        wazerMap["1"] = {x:0, y:62, width:67, height:51};
+        wazerMap["2"] = {x:151, y:0, width:52, height:48};
+        wazerMap["3"] = {x:151, y:48, width: 51, height:47};
+        wazerMap["4"] = {x:83, y:0, width:68, height:59};
+        wazerMap["5"] = {x:0, y:0, width:83, height:62};
+        wazerMap["6"] = {x:0, y:0, width:83, height:62};
+
+        WMEMe_Layer = newLayer;
+        var tween = new OpenLayers.Tween(OpenLayers.Easing.Linear.easeInOut);
+
+        I18n.translations[I18n.currentLocale()].layers.name['__WMEMe'] = 'Me!';
+
+        W.map.addLayer(newLayer);
+
+        newLayer.setVisibility(settings.layerEnabled);
+
+        WazeWrap.Interface.AddLayerCheckbox("display", "Me", settings.layerEnabled, function(enabled){newLayer.setVisibility(enabled); settings.layerEnabled = enabled; saveSettings();});
+
+        //image.setAttribute('crossOrigin', 'anonymous');
+        image.crossOrigin = "anonymous";
+        image.onload = setIconImage;
+
+        image.src = 'https://i.imgur.com/nFwdHlw.png';
 
         function tweenToPoint(e) {
             var newlonlat = W.map.center;
@@ -113,15 +152,7 @@
         W.map.events.register('zoomend', null, tweenToPoint);
         W.map.events.register('move', null, WMEMe_Drag);
         W.map.events.register('moveend', null, tweenToPoint);
-        W.map.events.register('movestart', null, function(e) {
-            var iconClone = icon.clone();
-            iconClone.setOpacity(0.2);
-            WMEMe_TempMarker = new OpenLayers.Marker(W.map.center, iconClone);
-            newLayer.addMarker(WMEMe_TempMarker);
-        });
 
-        WMEMe_Marker = new OpenLayers.Marker(W.map.center, icon);
-        newLayer.addMarker(WMEMe_Marker);
     }
 
     function SetName(ShowUsername){
